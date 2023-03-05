caef1d45d4b81abdda9edfcf04a9e8b3de9ec35b
Sudip Sarkar <sudipsarkar419@gmail.com>
2023-03-05 12:42:36 +0530

    initial commit

---
+++ config/key.js
@@ -0,0 +1,4 @@
+module.exports = {
+    // MongoURI:"mongodb+srv://sudip98:AlB8CRGDWhm8Kk91@cluster0.zsmd20g.mongodb.net/myFirstDatabase"
+    MongoURI:"mongodb+srv://sudip98:AlB8CRGDWhm8Kk91@cluster0.tkavhsq.mongodb.net/myFirstDatabase"
+}
